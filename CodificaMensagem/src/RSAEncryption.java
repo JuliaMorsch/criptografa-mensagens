b5818e8e247739e9354282e5fee82016eab32676
JULIA MORSCH <julia.morsch@alunos.sc.senac.br>
2024-09-06 10:50:33 -0300

    feat: Gerar par de chaves para criptografia e descriptografia de mensagem.

---
+++ CodificaMensagem/src/RSAEncryption.java
@@ -0,0 +1,11 @@
+import java.security.*;
+
+public class RSAEncryption {
+
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.genKeyPair();
+    }
+    
+}
