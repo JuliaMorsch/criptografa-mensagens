a7e487b37c5557f45f7b2a1e1f1763a3e2eb7822
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-12 00:37:13 -0300

    feat: Descriptografar a mensagem com a chave privada.

--- CodificaMensagem/src/RSAEncryption.java
+++ CodificaMensagem/src/RSAEncryption.java
@@ -1,4 +1,7 @@
 import java.security.*;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
 
 public class RSAEncryption {
 
@@ -6,6 +9,20 @@ public class RSAEncryption {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(2048);
         return keyGen.genKeyPair();
+    }
+
+    public static String encrypt(String message, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedMessage, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        return new String(decryptedBytes);
     }
     
 }
