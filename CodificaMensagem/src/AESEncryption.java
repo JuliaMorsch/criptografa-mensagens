a48d0710c085129b55ad3fce37092bf66bb4f14e
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-12 00:52:52 -0300

    feat: Descriptografar a mensagem com AES.

--- CodificaMensagem/src/AESEncryption.java
+++ CodificaMensagem/src/AESEncryption.java
@@ -1,3 +1,6 @@
+import java.util.Base64;
+
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
@@ -8,5 +11,11 @@ public class AESEncryption {
         keyGen.init(128);
         return keyGen.generateKey();
     }
-    
+
+    public static String encrypt(String message, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getEncoder().decode(encyptedMessage));
+        return new String(decryptedBytes);
+    }
 }
